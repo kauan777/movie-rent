7b7ff090f700deb04ef5d04c76c1ff1fb9f0230e
hemorgenes <kauan7622@gmail.com>
2022-11-23 16:40:44 -0300

    prettier

--- src/index.ts
+++ src/index.ts
@@ -8,20 +8,17 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
-app.use(
-  (err: Error, request: Request, response: Response, next: NextFunction) => {
-    if (err instanceof AppError) {
-      return response.status(err.statusCode).json({
-        status: "error",
-        message: err.message
-      });
-    }
-    return response.status(500).json({
-        status: "error",
-        message: `Internal server error - ${err.message}`
-    })
-
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      status: "error",
+      message: err.message,
+    });
   }
-);
+  return res.status(500).json({
+    status: "error",
+    message: `Internal server error - ${err.message}`,
+  });
+});
 
 app.listen(3333, () => console.log("🚀 Server is running in port 3333"));
