6abf055d45c1d52f46f76ded4a6c0f4286f6f57b
kauan777 <kauan7622@gmail.com>
2022-12-01 07:16:09 -0300

    add routes to get all usrs

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { CreateUserController } from "../modules/users/useCases/createUser/CreateUserController";
+import { GetAllUsersController } from "../modules/users/useCases/getAllUsers/GetAllUsersController";
 
 const createUserController = new CreateUserController();
+const getAllUsersController = new GetAllUsersController();
 
 const userRoutes = Router();
 
+userRoutes.get("/", getAllUsersController.handle);
 userRoutes.post("/", createUserController.handle);
 
 export { userRoutes };
